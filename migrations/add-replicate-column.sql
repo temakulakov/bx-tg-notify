97e79f01dd0a4bd429b3b9e92aecfda0bd9cd9d4
temakulakov <temakulakov100@gmail.com>
2025-11-13 07:56:18 +0300

    Update SQL migration script

--- migrations/add-replicate-column.sql
+++ migrations/add-replicate-column.sql
@@ -1,10 +1,7 @@
 -- Миграция: добавление колонки replicate в таблицу tasks
 -- Дата: 2025-11-10
-
 ALTER TABLE `tasks`
-ADD COLUMN `replicate` TINYINT(1) NOT NULL DEFAULT 0
-AFTER `created_by`;
+ADD COLUMN `replicate` TINYINT (1) NOT NULL DEFAULT 0 AFTER `created_by`;
 
 -- Обновление существующих записей (если нужно)
 -- По умолчанию все существующие задачи помечаются как нерегулярные (replicate = 0)
-
